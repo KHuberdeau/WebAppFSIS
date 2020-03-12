fb0b4dc0ff75e1fe6f42395678d4945862512ac8
Kalor Huberdeau <khuberdeau2@nait.ca>
2020-03-12 15:58:10 -0600

    a

--- WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
+++ WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FSISSystem.BLL;
+using FSISSystem.ENTITIES;
 
 namespace WebAppFSIS.ExercisePages
 {
@@ -11,7 +13,48 @@ namespace WebAppFSIS.ExercisePages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            MessageLabel.Text = "";
+        }
 
+        protected void Fetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TeamIDArg.Text))
+            {
+                MessageLabel.Text = "Enter a team id value.";
+            }
+            else
+            {
+                int teamid = 0;
+                if (int.TryParse(TeamIDArg.Text, out teamid))
+                {
+                    if (teamid > 0)
+                    {
+                        TeamController sysmgr = new TeamController();
+                        Team info = null;
+                        info = sysmgr.Teams_FindByID(teamid); //BLL controller method
+                        if (info == null)
+                        {
+                            MessageLabel.Text = "Team ID not found.";
+                            TeamID.Text = "";
+                            TeamDescription.Text = "";
+                        }
+                        else
+                        {
+                            TeamID.Text = info.TeamID.ToString();
+                            TeamDescription.Text = info.TeamDescription;
+                        }
+                    }
+                    else
+                    {
+                        MessageLabel.Text = "Team id must be greater than 0";
+                    }
+
+                }
+                else
+                {
+                    MessageLabel.Text = "Team id must be a number.";
+                }
+            }
         }
     }
 }
